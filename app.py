9f07bdc758768d1a5d4d2eb254af768fc49d7f22
arawesome1 <105249379+arawesome1@users.noreply.github.com>
2025-02-25 11:44:33 +0530

    Update app.py
    llama new model has been added , removing the preview model and whisper large v3 turbo added instead of mixtral preview model

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def generate_response(question,engine,temperature,max_token):
 
 
 st.title("QnA Chatbot")
-engine=st.sidebar.selectbox("Select Model",['gemma2-9b-it','lama3-groq-70b-8192-tool-use-preview','mixtral-8x7b-32768','llava-v1.5-7b-4096-preview'])
+engine=st.sidebar.selectbox("Select Model",['gemma2-9b-it','llama-3.1-8b-instant','mixtral-8x7b-32768','whisper-large-v3-turbo'])
 temperature = st.sidebar.slider("Temperature",min_value=0.0, max_value=1.0,value=0.7)
 max_token = st.sidebar.slider("Max Token",min_value=50, max_value=300, value = 150)
 
